b9989b84b3cd20cf5de013203f8f3d1a5eae2c82
Sewon <sewon1407@gmail.com>
2023-07-11 19:32:48 +0900

    Finished (ver 1)

--- app/src/main/java/sewon/bsis/hackathontest/MainActivity.kt
+++ app/src/main/java/sewon/bsis/hackathontest/MainActivity.kt
@@ -1,23 +1,20 @@
-package sewon.bsis.hackathontest
+@file:Suppress("DEPRECATION")
 
-//import com.spotify.protocol.client.Subscription
-//import com.spotify.protocol.types.PlayerState
+package sewon.bsis.hackathontest
 
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.system.Os.socket
+import android.util.Log
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
 import java.io.DataOutputStream
-import java.io.ObjectOutputStream
-import java.net.InetSocketAddress
 import java.net.Socket
 
 
@@ -25,109 +22,77 @@ class MainActivity : AppCompatActivity() {
 
     private val clientId = "a0c1fdefc1184a0ca550ac5162bb9c70"
     private val redirectUri = "http://localhost:8888/callback"
-//    private var spotifyAppRemote: SpotifyAppRemote? = null
 
-    private val REQUEST_CODE = 777
+    private val REQUEST_CODE_AUTH = 777
+    private val REQUEST_CODE_QR = 888
 
-    private lateinit var textView: TextView
-//    private lateinit var token_button: Button
     private lateinit var connect_button: Button
+
+    private lateinit var wifi_name: String
+    private lateinit var host_ip: String
+    private var token: String? = null
+
+    private val qrLauncher = registerForActivityResult(ScanContract()) {
+        if (it.contents == null)
+            Toast.makeText(this, "QR Null", Toast.LENGTH_SHORT).show()
+        else {
+            wifi_name = it.contents.split('\n')[0]
+            host_ip = it.contents.split('\n')[1]
+            Log.d("MainActivity", "wifi name: $wifi_name")
+            Log.d("MainActivity", "host_ip: $host_ip")
+
+            try {
+                Thread {
+                    val socket = Socket(host_ip, 59876)
+                    val stream = DataOutputStream(socket.getOutputStream())
+                    stream.writeUTF(token)
+                }.start()
+            } catch (_: Exception) {
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.textView)
+        Toast.makeText(this, "Spotify와 연결 중입니다. 기다려주세요.", Toast.LENGTH_LONG).show()
+        val builder =
+            AuthorizationRequest.Builder(clientId, AuthorizationResponse.Type.TOKEN, redirectUri)
+        builder.setScopes(arrayOf("user-top-read", "user-follow-read", "user-read-private"))
+        val request = builder.build()
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE_AUTH, request)
+
         connect_button = findViewById(R.id.connect_btn)
-
         connect_button.setOnClickListener {
-            val builder =
-                AuthorizationRequest.Builder(clientId, AuthorizationResponse.Type.TOKEN, redirectUri)
-            builder.setScopes(
-                arrayOf("user-top-read", "user-follow-read", "user-read-private")
-//                arrayOf("streaming,user-read-private,user-read-email,playlist-read-private,playlist-read-collaborative,user-follow-read,user-read-playback-position,user-top-read,user-read-recently-played")
-            )
-            val request = builder.build()
-            AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request)
-
-            val handler = it.handler
-            Thread {
-                try {
-                    val socket = Socket("192.168.26.159",59876)
-                    val stream = DataOutputStream(socket.getOutputStream())
-                    stream.writeUTF("Passione rocks!")
-                } catch (e: Exception) {
-                    handler.post {
-                        Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }.start()
+            if (token == null) {
+                Toast.makeText(this, "token is null", Toast.LENGTH_SHORT).show()
+            } else {
+                qrLauncher.launch(ScanOptions())
+            }
         }
     }
-
-    override fun onStart() {
-        super.onStart()
-        ////////////////////////////////////////////////////////////
-//        val connectionParams = ConnectionParams.Builder(clientId)
-//            .setRedirectUri(redirectUri)
-//            .showAuthView(true)
-//            .build()
-//
-//        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
-//            override fun onConnected(appRemote: SpotifyAppRemote) {
-//                spotifyAppRemote = appRemote
-//                Log.d("MainActivity", "Connected! Yay!")
-//                // Now you can start interacting with App Remote
-//                connected()
-//            }
-//
-//            override fun onFailure(throwable: Throwable) {
-//                Log.e("MainActivity", throwable.message, throwable)
-//                // Something went wrong when attempting to connect! Handle errors here
-//            }
-//        })
-    }
-//
-//    private fun connected() {
-//        spotifyAppRemote?.let {
-//            // Play a playlist
-//            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
-//            it.playerApi.play(playlistURI)
-//            // Subscribe to PlayerState
-//            it.playerApi.subscribeToPlayerState().setEventCallback {
-//                val track: Track = it.track
-//                Log.d("MainActivity", track.name + " by " + track.artist.name)
-//            }
-//        }
-//
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        spotifyAppRemote?.let {
-//            SpotifyAppRemote.disconnect(it)
-//        }
-//    }
 
     @SuppressLint("SetTextI18n")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode != RESULT_OK) return
 
-        if (requestCode == REQUEST_CODE) {
+        if (requestCode == REQUEST_CODE_AUTH) {
             val response = AuthorizationClient.getResponse(resultCode, data)
-            when (val type = response.type) {
+            val type = response.type
+            when (type) {
                 AuthorizationResponse.Type.TOKEN -> {
-                    textView.text = response.accessToken
+                    token = response.accessToken
                 }
 
-                AuthorizationResponse.Type.ERROR -> {
-                    textView.text = "Error!"
-                }
+                AuthorizationResponse.Type.ERROR -> Toast.makeText(
+                    this,
+                    "Error while getting Spotify token.",
+                    Toast.LENGTH_SHORT
+                ).show()
 
-                else -> {
-                    textView.text = "??? Something's wrong"
-                }
+                else -> {}
             }
         }
     }
